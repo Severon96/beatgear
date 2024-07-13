5f1462e9890e5fa9492f06e08f8f694a5eaa1a08
Dominik Mack <dominik.mack@icloud.com>
2024-07-13 10:20:32 +0200

    Added users api

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,9 @@
 import psycopg2.extras
 from chalice import Chalice
 
-from api.api_buildings import api as api_buildings
-from api.api_organisations import api as api_organisations
-from api.api_rooms import api as api_rooms
-from api.api_appointments import api as api_appointments
-from api.api_swaggerui import api as api_swaggerui
-from api.api_events import api as api_events
-from util.mock_data import generate_mock_data
+from api.api_users import api as api_users
 
-app = Chalice("dashboard-backend")
+app = Chalice("backend")
 
 
 @app.route("/")
@@ -18,11 +12,4 @@ def hello_world():
 
 
 psycopg2.extras.register_uuid()
-app.register_blueprint(api_organisations, url_prefix="/api")
-app.register_blueprint(api_buildings, url_prefix="/api")
-app.register_blueprint(api_rooms, url_prefix="/api")
-app.register_blueprint(api_appointments, url_prefix="/api")
-app.register_blueprint(api_swaggerui, url_prefix="/api")
-app.register_blueprint(api_events, url_prefix="/api")
-
-generate_mock_data()
+app.register_blueprint(api_users, url_prefix="/api")
