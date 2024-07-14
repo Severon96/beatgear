06530464d0880c95076396ee14edc69e78ac37c3
Dominik Mack <dominik.mack@icloud.com>
2024-07-14 10:18:58 +0200

    New database tables for hardware and bookings

--- backend/sql/postgres_init.sql
+++ backend/sql/postgres_init.sql
@@ -2,11 +2,34 @@ CREATE DATABASE hardware_management;
 
 \c hardware_management;
 
+CREATE TYPE hardware_category AS ENUM ('controller', 'light', 'cable-xlr', 'plug-cold-appliance', 'laptop-stand', 'other');
+
 CREATE TABLE IF NOT EXISTS users (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS hardware (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    serial VARCHAR(255) NOT NULL,
+    image bytea,
+    category hardware_category NOT NULL,
+    owner UUID REFERENCES hardware(id),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS booking(
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    customer_id UUID REFERENCES users(id) NOT NULL,
+    hardware_id UUID REFERENCES hardware(id) NOT NULL,
+    booking_start TIMESTAMP NOT NULL,
+    booking_end TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
