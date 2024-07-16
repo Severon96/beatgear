4e74850849671ae138b1b0f84caac25bf6101bb1
Dominik Mack <dominik.mack@icloud.com>
2024-07-16 18:48:50 +0200

    Started working on booking db access

--- backend/db/bookings_db.py
+++ backend/db/bookings_db.py
@@ -1,7 +1,9 @@
+import uuid
+from datetime import datetime
 from uuid import UUID
 
-from chalice import NotFoundError
-from sqlalchemy import select
+from chalice import NotFoundError, BadRequestError
+from sqlalchemy import select, insert
 
 from models.models import Booking
 from util.util import get_db_connection
@@ -20,25 +22,34 @@ def get_booking(booking_id: UUID) -> Booking:
 
 
 def get_all_bookings() -> list[Booking]:
-    connection, cursor = get_db_connection()
+    connection = get_db_connection()
 
-    cursor.execute("SELECT * FROM bookings;")
-    values = cursor.fetchall()
+    statement = select(Booking)
+    rows = connection.execute(statement).scalars()
 
-    close_db_connection(connection, cursor)
-
-    return parse_model_list(Booking, values)
+    return [row for row in rows]
 
 
 def create_booking(booking: Booking) -> Booking:
-    connection, cursor = get_db_connection()
+    connection = get_db_connection()
 
-    cursor.execute(
-        "INSERT INTO bookings(id, title, room_id, start_time, end_time, recurring) VALUES (%s, %s, %s, %s, %s, %s)",
-        (str(booking.id), booking.title, str(booking.room_id), booking.start_time, booking.end_time,
-         booking.recurring))
-    connection.commit()
+    statement = insert(Booking).values(
+        id=uuid.uuid4(),
+        name=booking.name,
+        customer_id=booking.customer_id,
+        hardware_id=booking.hardware_id,
+        booking_start=booking.booking_start,
+        booking_end=booking.booking_end,
+        created_at=datetime.now(),
+        updated_at=datetime.now()
+    ).returning(Booking.id)
+    statement.compile()
 
-    close_db_connection(connection, cursor)
+    rows = connection.execute(statement)
 
-    return booking
+    inserted_row_id = rows.first()
+    print(inserted_row_id)
+    if inserted_row_id is None:
+        raise BadRequestError("Error inserting new booking")
+
+    return None
