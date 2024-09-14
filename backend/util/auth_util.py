cc591a0fac0b8a375c6002abaee9a3c84822e912
Dominik Mack <dominik.mack@icloud.com>
2024-09-14 13:31:51 +0200

    Fixed container startup with proper keycloak address in default .env

--- backend/util/auth_util.py
+++ backend/util/auth_util.py
@@ -1,5 +1,7 @@
 import json
+import os
 from functools import wraps
+from time import sleep
 from uuid import UUID
 
 import dotenv
@@ -7,22 +9,25 @@ import requests
 from flask import jsonify
 from flask_jwt_extended import verify_jwt_in_request, get_jwt
 from jwt.algorithms import RSAAlgorithm
-from requests import JSONDecodeError
 
 from models.request_models import AuthenticatedUser
 
 
-def fetch_public_key(oauth_issuer: str, realm_name: str) -> str:
-    response = requests.get(
+def fetch_public_key(oauth_issuer: str, realm_name: str, retries: int = 1) -> str:
+    max_request_retries = os.environ.get('MAX_REQUEST_RETRIES', 10)
+    max_request_retries_wait = os.environ.get('MAX_REQUEST_RETRY_WAIT_IN_S', 5)
+
+    try:
+        response = requests.get(
         f"{oauth_issuer}/realms/{realm_name}/.well-known/openid-configuration",
         headers={"Content-Type": "application/json"},
         verify=False
-    )
+        )
 
-    if response.status_code != 200:
-        print(f"failed fetching public key: {response.status_code}", response.text)
-        raise ValueError(f"Couldn't fetch openid configuration: {response.status_code}")
-    try:
+        if response.status_code != 200:
+            print(f"failed fetching public key: {response.status_code}", response.text)
+            raise ValueError(f"Couldn't fetch openid configuration: {response.status_code}")
+
         oidc_config = response.json()
 
         oidc_jwks_uri = requests.get(oidc_config["jwks_uri"], verify=False).json()
@@ -30,10 +35,15 @@ def fetch_public_key(oauth_issuer: str, realm_name: str) -> str:
         return RSAAlgorithm.from_jwk(
             json.dumps(oidc_jwks_uri["keys"][0])
         )
-    except JSONDecodeError as e:
-        print(f"failed fetching public key: {e}", e.response)
-        raise ValueError(f"Couldn't fetch public key: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"failed fetching public key because keycloak not available: {e}", e)
 
+        if retries <= int(max_request_retries):
+            sleep(int(max_request_retries_wait))
+            print(f"retrying fetch for the {retries} time")
+            fetch_public_key(oauth_issuer, realm_name, retries + 1)
+        else:
+            raise e
 
 
 def token_required(fn):
