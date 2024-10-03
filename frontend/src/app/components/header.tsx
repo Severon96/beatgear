a2375db51097c4d4a4b10976cfac9a7571125a33
Dominik Mack <dominik.mack@icloud.com>
2024-10-03 13:37:08 +0200

    Completed header

--- frontend/src/app/components/header.tsx
+++ frontend/src/app/components/header.tsx
@@ -4,19 +4,24 @@ import {metadata} from "@/app/layout";
 
 export function Header() {
     return (
-        <div className={`w-screen grid flex-col grid-cols-4`}>
-            <div className={"grid flex-col grid-cols-2"}>
+        <div className="w-full grid grid-cols-2 relative">
+            <div className="flex items-center space-x-2 relative">
                 <Image
                     src="/images/logo.jpeg"
                     width={50}
                     height={50}
                     alt="BeatGear Logo"
                 />
-                <div className={"flex h-full font-[family-name:var(--font-outfit)]"}>
-                    <span className={"content-center"}>{`${metadata.title}`}</span>
-                </div>
+                <span>{`${metadata.title}`}</span>
             </div>
-
+            <div className="flex items-center ml-auto space-x-2 relative">
+                <a href={"login"} className={"button-secondary-blue py-1 px-3 rounded-lg"}>
+                    Login
+                </a>
+                <a href={"register"} className={"button-secondary-grey py-1 px-3 rounded-lg"}>
+                    Register
+                </a>
+            </div>
         </div>
     );
 }
