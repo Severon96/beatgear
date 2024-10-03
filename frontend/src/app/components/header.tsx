6becab363549aa6082070954ac82b5c5e75f7607
Dominik Mack <dominik.mack@icloud.com>
2024-10-03 18:36:50 +0200

    Finished header

--- frontend/src/app/components/header.tsx
+++ frontend/src/app/components/header.tsx
@@ -4,8 +4,8 @@ import {metadata} from "@/app/layout";
 
 export function Header() {
     return (
-        <div className="w-full grid grid-cols-2 relative">
-            <div className="flex items-center space-x-2 relative">
+        <div className="w-full flex flex-col md:flex-row justify-between items-center pt-16 pb-2 md:pt-4 md:mt-0 border-b-2">
+            <div className="flex items-center space-x-2">
                 <Image
                     src="/images/logo.jpeg"
                     width={50}
@@ -14,12 +14,12 @@ export function Header() {
                 />
                 <span>{`${metadata.title}`}</span>
             </div>
-            <div className="flex items-center ml-auto space-x-2 relative">
-                <a href={"login"} className={"button-secondary-blue py-1 px-3 rounded-lg"}>
-                    Login
+            <div className="flex items-center space-x-2 mt-4 md:mt-0">
+                <a href="login" className="button-secondary-blue py-1 px-3 rounded-lg">
+                    Anmelden
                 </a>
-                <a href={"register"} className={"button-secondary-grey py-1 px-3 rounded-lg"}>
-                    Register
+                <a href="register" className="button-secondary-grey py-1 px-3 rounded-lg">
+                    Registrieren
                 </a>
             </div>
         </div>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import {Header} from "@/app/components/header";
 export default function Home() {
     return (
         <div
-            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)] overflow-x-hidden">
+            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-16 font-[family-name:var(--font-geist-sans)] overflow-x-hidden pt-4">
             <main className="flex flex-col gap-8 items-center sm:items-start w-full">
                 <header className="w-full">
                     <Header/>
