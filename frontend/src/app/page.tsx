fcc997f941db898e54f8e2d00a82ce8156f02e19
Dominik Mack <dominik.mack@icloud.com>
2024-10-04 11:08:38 +0200

    Add frontend react project

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import {Header} from "@/app/components/header";
+import {Header} from "@/app/pages/components/header";
 import {isLoggedIn} from "@/app/utils/auth";
-import {LandingPage} from "@/app/components/landing-page";
+import {LandingPage} from "@/app/pages/components/landing-page";
 
 export default async function Home() {
     return (
         <div
-            className="items-center justify-items-center min-h-screen pb-20 gap-16 font-[family-name:var(--font-geist-sans)] overflow-x-hidden relative">
+            className="items-center justify-items-center min-h-screen pb-20 gap-16 font-[family-name:var(--font-geist-sans)] overflow-x-hidden relative" suppressHydrationWarning={true}>
             <main className="flex flex-col gap-8 items-center sm:items-start w-full relative">
                 <header className="w-full relative">
                     <Header/>
