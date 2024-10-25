c4493cda7166339651807e4f7538c869165ba47e
Dominik Mack <dominik.mack@icloud.com>
2024-10-25 23:24:52 +0200

    Add card for creating bookings

--- frontend/src/components/ActiveBookings.tsx
+++ frontend/src/components/ActiveBookings.tsx
@@ -1,9 +1,10 @@
 import React, {useEffect} from "react";
 import Box from "@mui/material/Box";
-import {Card, CardHeader} from "@mui/material";
+import {Card, CardContent, CardHeader, Stack} from "@mui/material";
 import {fetchActiveBookings} from "../redux-tk/slices/bookingSlice";
 import {useAppDispatch, useAppSelector} from "../store";
 import Typography from "@mui/material/Typography";
+import {Popup} from "./Popup";
 
 export function ActiveBookings() {
     const dispatch = useAppDispatch();
@@ -13,17 +14,24 @@ export function ActiveBookings() {
         return (
             <Card>
                 <CardHeader title={"Buchungen"}/>
-                {
-                    activeBookings.length > 0 ? (
-                        <Box>
-                            <p>{`Bookings of user`}</p>
-                            <p>Current active bookings</p>
-                        </Box>
-                    ) : (
-                        <Typography variant={'body1'}>You have no bookings, feel free to create one!</Typography>
-                    )
-                }
-
+                <CardContent>
+                    {
+                        activeBookings.length > 0 ? (
+                            <Box>
+                                <p>{`Bookings of user`}</p>
+                                <p>Current active bookings</p>
+                            </Box>
+                        ) : (
+                            <Stack alignItems={"center"}>
+                                <Typography variant={'subtitle1'}>You have no bookings, feel free to create
+                                    one!</Typography>
+                                <Popup buttonName={"Booking anlegen"}>
+                                    <Typography>Popup Inhalt</Typography>
+                                </Popup>
+                            </Stack>
+                        )
+                    }
+                </CardContent>
             </Card>
         )
     }
