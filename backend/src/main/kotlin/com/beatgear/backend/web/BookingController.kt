10e5876296f1263db9b321aa223fa88398bea77e
Dominik Mack <dominik.mack@icloud.com>
2024-12-13 22:00:54 +0100

    Added mapper for DTOs

--- backend/src/main/kotlin/com/beatgear/backend/web/BookingController.kt
+++ backend/src/main/kotlin/com/beatgear/backend/web/BookingController.kt
@@ -1,15 +1,19 @@
 package com.beatgear.backend.web
 
-import com.beatgear.backend.model.Booking
+import com.beatgear.backend.dto.BookingDto
+import com.beatgear.backend.mapper.BookingMapper
 import com.beatgear.backend.service.BookingService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController(value = "/bookings")
-class BookingController(private val bookingService: BookingService) {
+class BookingController(
+    private val bookingService: BookingService,
+    private val bookingMapper: BookingMapper
+) {
 
     @GetMapping("/current")
-    fun getCurrentBookings(): List<Booking> {
-        return bookingService.getCurrentBookings()
+    fun getCurrentBookings(): List<BookingDto> {
+        return bookingService.getCurrentBookings().map { bookingMapper.mapToBookingDto(it) }
     }
 }
