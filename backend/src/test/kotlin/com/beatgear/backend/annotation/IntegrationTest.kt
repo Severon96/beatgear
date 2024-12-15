150abe7344d6c88024badd592a2d41838e2c1517
Dominik Mack <dominik.mack@icloud.com>
2024-12-15 20:11:28 +0100

    Remove test initializier

--- backend/src/test/kotlin/com/beatgear/backend/annotation/IntegrationTest.kt
+++ backend/src/test/kotlin/com/beatgear/backend/annotation/IntegrationTest.kt
@@ -1,10 +1,9 @@
 import io.restassured.RestAssured
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.util.TestPropertyValues
-import org.springframework.context.ApplicationContextInitializer
-import org.springframework.context.ConfigurableApplicationContext
-import org.springframework.test.context.*
-import org.testcontainers.containers.PostgreSQLContainer
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestContext
+import org.springframework.test.context.TestExecutionListener
+import org.springframework.test.context.TestExecutionListeners
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @Target(AnnotationTarget.CLASS)
@@ -12,30 +11,11 @@ import org.testcontainers.junit.jupiter.Testcontainers
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
-@ContextConfiguration(initializers = [IntegrationTest.Initializer::class])
 @TestExecutionListeners(
     listeners = [IntegrationTest.ExecutionListener::class],
     mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS
 )
 annotation class IntegrationTest {
-    class Initializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
-
-        override fun initialize(applicationContext: ConfigurableApplicationContext) {
-            TestPropertyValues.of(
-                "spring.datasource.url=${postgres.jdbcUrl}",
-                "spring.datasource.username=${postgres.username}",
-                "spring.datasource.password=${postgres.password}",
-            ).applyTo(applicationContext.environment)
-        }
-
-        companion object {
-            val postgres =
-                PostgreSQLContainer<Nothing>("postgres:16-alpine")
-                    .apply { withReuse(true) }
-                    .apply { start() }
-        }
-    }
-
     class ExecutionListener : TestExecutionListener {
         override fun beforeTestMethod(testContext: TestContext) {
             val applicationContext = testContext.applicationContext
